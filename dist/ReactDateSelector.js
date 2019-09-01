0bf1efa0dfadb2e8d8c735fe7f9739e5eadac1e5
Adarsh Pastakia <adarshpastakia@outlook.com>
2019-09-01 20:21:43 +0400

    Initial dist

--- dist/ReactDateSelector.js
+++ dist/ReactDateSelector.js
@@ -13,8 +13,6 @@ var _RdsInput = require("./components/RdsInput");
 
 var _antd = require("antd");
 
-var _typestyle = require("typestyle");
-
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
@@ -30,21 +28,6 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
 
 function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
-var css = (0, _typestyle.stylesheet)({
-  dropdown: {
-    padding: 0,
-    zIndex: 1050,
-    $nest: {
-      "& .ant-popover-arrow": {
-        display: "none"
-      },
-      "& .ant-popover-inner-content": {
-        padding: 0
-      }
-    }
-  }
-});
 
 var ReactDateSelector = _react.default.forwardRef(function (_ref, ref) {
   var onDateChange = _ref.onDateChange,
@@ -82,7 +65,7 @@ var ReactDateSelector = _react.default.forwardRef(function (_ref, ref) {
     placement: "bottomLeft",
     visible: isOpen,
     onVisibleChange: changeVisible,
-    overlayClassName: css.dropdown,
+    overlayClassName: "ards-dropdown-overlay",
     content: isOpen && _react.default.createElement(_RdsDropdown.RdsDropdown, _extends({}, props, {
       onDateChange: updateValue
     }))
