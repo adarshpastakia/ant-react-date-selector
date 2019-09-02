d250ea49b2289bd5fbb8740ec2919b0bd203ba64
Adarsh Pastakia <adarshpastakia@outlook.com>
2019-09-02 12:24:59 +0400

    Demo build

---
+++ docs/precache-manifest.9eeee03e02ae0b30b46ed276fd1727f0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f0b7a89cc2675686bacb",
+    "url": "/static/js/main.392bfec1.chunk.js"
+  },
+  {
+    "revision": "1dd446bc9900cb522176",
+    "url": "/static/js/2.4d1e4d74.chunk.js"
+  },
+  {
+    "revision": "aa678041334271ae645cd32807eea84f",
+    "url": "/index.html"
+  }
+];
